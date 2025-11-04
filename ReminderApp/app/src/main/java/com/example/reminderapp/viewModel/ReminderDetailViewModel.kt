b227ddb986549df8d766632e9823109255b13d3e
melvinmalongamatouba <melvinmalongamatouba@gmail.com>
2025-11-04 14:00:19 +0100

    ReminderDetailViewModel.kt

--- ReminderApp/app/src/main/java/com/example/reminderapp/viewModel/ReminderDetailViewModel.kt
+++ ReminderApp/app/src/main/java/com/example/reminderapp/viewModel/ReminderDetailViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.reminderapp.viewModel
 
+import androidx.lifecycle.ViewModel
+import com.example.reminderapp.Reminder
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class ReminderDetailViewModel : ViewModel(){
+    private val _reminder : MutableStateFlow<Reminder?> = MutableStateFlow(null)
+    val reminder : StateFlow<Reminder?> = _reminder
+    fun setReminder(reminder: Reminder) {
+        _reminder.value = reminder
+    }
+
+
+}
+
