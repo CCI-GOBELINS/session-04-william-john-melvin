b5f3cf519320eb8bc6a662d8d2ee6e354e03454f
melvinmalongamatouba <melvinmalongamatouba@gmail.com>
2025-11-05 14:52:56 +0100

    Update ReminderRepo.kt
    Di Injection Repository

--- ReminderApp/app/src/main/java/data/repository/ReminderRepo.kt
+++ ReminderApp/app/src/main/java/data/repository/ReminderRepo.kt
@@ -2,7 +2,10 @@ package data.repository
 import com.example.reminderapp.Reminder
 import com.example.reminderapp.database.ReminderDatabase
 import kotlinx.coroutines.flow.Flow import kotlinx.coroutines.flow.flow
-class ReminderRepository(val database: ReminderDatabase) {
+import javax.inject.Inject
+
+class ReminderRepository @Inject constructor
+    (val database: ReminderDatabase) {
 
     suspend fun getAllReminders(): Flow<List<Reminder>> = flow{
         val scores: List<Reminder> = database.reminderDao().getAllReminders()
@@ -16,10 +19,11 @@ class ReminderRepository(val database: ReminderDatabase) {
     companion object {
         @Volatile
         private var _inst: ReminderRepository? = null
-            val instance: ReminderRepository
-                get() = _inst!!
 
-        fun getInstance(): ReminderRepository {
+        val instance: ReminderRepository
+            get() = _inst!!
+
+        fun initRepository(): ReminderRepository {
             return _inst ?: synchronized(lock = this) {
                 val instance = ReminderRepository(ReminderDatabase.instance)
                 _inst = instance
