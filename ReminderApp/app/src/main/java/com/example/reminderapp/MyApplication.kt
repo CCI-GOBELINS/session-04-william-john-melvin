67d7074ed911b302c401ac128dbd78bb4eb000be
melvinmalongamatouba <melvinmalongamatouba@gmail.com>
2025-11-05 14:31:14 +0100

    Update MyApplication.kt
    Added HiltAndroidApp

--- ReminderApp/app/src/main/java/com/example/reminderapp/MyApplication.kt
+++ ReminderApp/app/src/main/java/com/example/reminderapp/MyApplication.kt
@@ -2,7 +2,9 @@ package com.example.reminderapp
 
 import android.app.Application
 import com.example.reminderapp.database.ReminderDatabase
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
