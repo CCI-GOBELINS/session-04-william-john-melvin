4f148712e34978a6e7aadf67c82294abb5a5356c
melvinmalongamatouba <melvinmalongamatouba@gmail.com>
2025-11-05 14:29:45 +0100

    Updated gradle and toml to add hilt

--- ReminderApp/app/build.gradle.kts
+++ ReminderApp/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -47,8 +48,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
-    implementation ("androidx.navigation:navigation-compose:2.9.5")
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation (libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
@@ -63,5 +64,8 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+    implementation(libs.hilt.android)
+    ksp(libs.dagger.hilt.android.compiler)
+
 
 }
